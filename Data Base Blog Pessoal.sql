6a3653a772b43c3d424c091ff7068e07adceee30
Luis Henrique <selesluishenrique@gmail.com>
2022-04-28 21:20:03 -0300

    Inserindo dados no Banco de Dados Blog Pessoal

---
+++ Data Base Blog Pessoal.sql
@@ -0,0 +1,13 @@
+
+
+INSERT tb_postagem (texto, titulo)
+VALUES ("Estou aprendendo", "API Rest Spring");
+
+INSERT tb_postagem (texto, titulo)
+VALUES ("Olá, sou o Luis Henrique", "mas você já sabe!");
+
+INSERT tb_postagem (texto, titulo)
+VALUES ("Blog pessoal em construção...", "Blog pessoal");
+
+select*from tb_postagem;
+
