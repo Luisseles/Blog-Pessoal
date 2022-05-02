20972a28c68f2652ddc114123068d61a1f0aa473
Luis Henrique <selesluishenrique@gmail.com>
2022-05-02 16:06:14 -0300

    Back-end blog-pessoal...

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -6,9 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity /* transforma o objeto criado em uma tabela no banco de dados*/
 // dá um nome para a tabela no meu banco de dados.
@@ -32,10 +35,22 @@ public class Postagem {
 	@NotNull 
 	public String texto;
 	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 	public Date date = new java.sql.Date(System.currentTimeMillis()) ;
 
 	public Date getDate() {
 		return date;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 
 	public void setDate(Date date) {
